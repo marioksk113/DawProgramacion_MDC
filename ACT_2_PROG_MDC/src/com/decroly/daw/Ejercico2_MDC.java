ab084ea9514c3b40a172c0c1f96c2e4787d4178c
COMPU-S023\daw1 <mcarlosi01@educantabria.es>
2024-10-03 09:16:19 +0200

    3/10

--- ACT_2_PROG_MDC/src/com/decroly/daw/Ejercico2_MDC.java
+++ ACT_2_PROG_MDC/src/com/decroly/daw/Ejercico2_MDC.java
@@ -130,5 +130,273 @@ public class Ejercico2_MDC {
      System.out.println("El factorial de " + num + " es: " + factorial);
      
 	    }
-	}
-}
+
+	// 9
+
+	scanner = new Scanner(System.in);
+
+
+	System.out.print("Introduce la hora: ");
+
+		int hora = scanner.nextInt();
+
+	System.out.print("Introduce los minutos: ");
+
+		int minuto = scanner.nextInt();
+
+	System.out.print("Introduce los segundos: ");
+
+		int segundo = scanner.nextInt();
+
+		segundo++;
+
+		if (segundo == 60) {
+			segundo = 0;
+			minuto++;
+		}
+		if (minuto == 60) {
+			minuto = 0;
+			hora++;
+		}
+		if (hora == 24) {
+			hora = 0;
+		}
+
+	System.out.printf("La hora pasado un segundo es: %02d:%02d:%02d\n", hora, minuto, segundo);
+
+
+			
+	// 10
+
+	scanner = new Scanner(System.in);
+
+	System.out.println("Numero 1: ");
+
+		int n1 = scanner.nextInt();
+	
+	System.out.println("Numero 2: ");
+
+		int n2 = scanner.nextInt();
+	
+	System.out.println("Numero 3: ");
+
+		int n3 = scanner.nextInt();
+	
+	System.out.println("Numero 4: ");
+
+		int n4 = scanner.nextInt();
+
+	System.out.println("Numero 5: ");
+
+		int n5 = scanner.nextInt();
+	
+	System.out.println("Numero 6: ");
+
+		int n6 = scanner.nextInt();
+	
+	System.out.println("Numero 7: ");
+
+		int n7 = scanner.nextInt();
+	
+	System.out.println("Numero 8: ");
+
+		int n8 = scanner.nextInt();
+
+	System.out.println("Numero 9: ");
+
+		int n9 = scanner.nextInt();
+	
+	System.out.println("Numero 10: ");
+
+		int n10 = scanner.nextInt();
+
+		if (n1 < 0 || n2 < 0 || n3 < 0 || n4 < 0 || n5 < 0 || 
+			n6 < 0 || n7 < 0 || n8 < 0 || n9 < 0 || n10 < 0) {
+			
+			System.out.print("Hay numeros negativos");
+		} else {
+			System.out.println("No hay numeros negativos");
+		}
+
+	// 11
+
+	scanner = new Scanner(System.in);
+
+	System.out.println("Numero 1: ");
+
+		int nm1 = scanner.nextInt();
+	
+	System.out.println("Numero 2: ");
+
+		int nm2 = scanner.nextInt();
+	
+	System.out.println("Numero 3: ");
+
+		int nm3 = scanner.nextInt();
+	
+	System.out.println("Numero 4: ");
+
+		int nm4 = scanner.nextInt();
+
+	System.out.println("Numero 5: ");
+
+		int nm5 = scanner.nextInt();
+	
+	System.out.println("Numero 6: ");
+
+		int nm6 = scanner.nextInt();
+	
+	System.out.println("Numero 7: ");
+
+		int nm7 = scanner.nextInt();
+	
+	System.out.println("Numero 8: ");
+
+		int nm8 = scanner.nextInt();
+
+	System.out.println("Numero 9: ");
+
+		int nm9 = scanner.nextInt();
+	
+	System.out.println("Numero 10: ");
+
+		int nm10 = scanner.nextInt();
+
+		
+		int positivos = 0;
+		int negativos = 0;
+		
+		if (nm1 < 0) {
+
+			positivos++;
+
+		} else {
+			negativos++;
+		}
+
+		if (nm2 < 0) {
+
+			positivos++;
+
+		} else {
+			negativos++;
+		}
+
+		if (nm3 < 0) {
+
+			positivos++;
+
+		} else {
+			negativos++;
+		}
+
+		if (nm4 < 0) {
+
+			positivos++;
+
+		} else {
+			negativos++;
+		}
+
+		if (nm5 < 0) {
+
+			positivos++;
+
+		} else {
+			negativos++;
+		}
+
+		if (nm6 < 0) {
+
+			positivos++;
+
+		} else {
+			negativos++;
+		}
+
+		if (nm7 < 0) {
+
+			positivos++;
+
+		} else {
+			negativos++;
+		}
+
+		if (nm8 < 0) {
+
+			positivos++;
+
+		} else {
+			negativos++;
+		}
+
+		if (nm9 < 0) {
+
+			positivos++;
+
+		} else {
+			negativos++;
+		}
+
+		if (nm10 < 0) {
+
+			positivos++;
+
+		} else {
+			negativos++;
+		}
+	
+	System.out.println("Cantidad de numeros positivos: "+positivos);
+	System.out.println("Cantidad de numeros negativos: "+negativos);
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}//main
+
+}//clase
+
