8a1d06322056f266c3709d32db2ce864c4b93381
COMPU-S023\daw1 <mcarlosi01@educantabria.es>
2025-02-03 12:55:21 +0100

    dd

--- Colecciones_MDC/Agenda/src/App.java
+++ Colecciones_MDC/Agenda/src/App.java
@@ -36,7 +36,6 @@ public class App {
                     int resultado = agenda.añadirContacto(nombre, telefono, correo);
                     switch(resultado) {
                         case 0:
-                        
                             System.out.println("Contacto añadido correctamente");
                             break;
                         case 1:
